2cf2e5419ec6bbde6dbc17292ec5e6c989decc05
flisoch <flisochcs@gmail.com>
2021-03-29 22:23:55 +0300

    rename 1 task folder

---
+++ task1 - javaCrawler/src/main/java/ru/itis/Main.java
@@ -0,0 +1,11 @@
+package ru.itis;
+
+import ru.itis.crawler.Crawler;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Crawler crawler = new Crawler();
+        crawler.start();
+    }
+}
