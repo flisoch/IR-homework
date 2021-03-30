d03bca2dd671e2c2362f26f5cf7e817e4e143804
flisoch <flisochcs@gmail.com>
2021-03-30 15:15:41 +0300

    add boolean search

--- task3 - inverted index/Task3.jl
+++ task3 - inverted index/Task3.jl
@@ -4,8 +4,20 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
 # ╔═╡ 9170c1a6-913f-11eb-0d5e-41f097fd770f
 using TextAnalysis
+
+# ╔═╡ f70b8200-914c-11eb-1e9e-f31b3927d722
+using PlutoUI
 
 # ╔═╡ a36cdab6-913f-11eb-3356-c979b7b5c91f
 include("task2 - tokens/Task2.jl")
@@ -57,8 +69,71 @@ end
 # ╔═╡ d18d5204-9147-11eb-102d-c1c036f4330d
 saveindex(invidx)
 
+# ╔═╡ 60550df8-914d-11eb-2eba-992e3beb53dd
+@bind query TextField()
+
+# ╔═╡ 6f655a78-914d-11eb-2769-dd25486211e3
+splitquery = split(query)
+
+# ╔═╡ d7ac1296-914d-11eb-359e-03a5a5b346a7
+command = splitquery[2]
+
+# ╔═╡ 9d3e7646-914d-11eb-1a72-6ffa5fb430fb
+invidx[splitquery[1]]
+
+# ╔═╡ eae3e746-914d-11eb-11d2-9f4bc8e0fdf9
+totalnums = []
+
+# ╔═╡ 05d0beb2-914e-11eb-2203-f792beaa3ac0
+word1 = splitquery[1]
+
+# ╔═╡ 0d796f24-914e-11eb-044e-a3c821a44dd9
+word2 = splitquery[3]
+
+# ╔═╡ 639624ce-914e-11eb-3e9f-d97221ea109e
+function bothwords(invidx, word1, word2)
+	list1 = invidx[word1]
+	list2 = invidx[word2]
+	intersect(list1, list2)
+end
+
+# ╔═╡ 58b4c9ce-914f-11eb-3cbf-99c436e2e293
+function withoutsecond(invidx, word1, word2)
+	list1 = invidx[word1]
+	list2 = invidx[word2]
+	both = unique([list1; list2])
+	findall(x -> (x in list1) && !(x in list2), both)
+end
+
+# ╔═╡ bceedcd6-914f-11eb-3337-954c38a501ae
+function anyword(invidx, word1, word2)
+	list1 = invidx[word1]
+	list2 = invidx[word2]
+	unique([list1; list2])
+end
+
+# ╔═╡ e122ed88-914d-11eb-0350-212db87d2f94
+function booleansearch(invidx, word1, word2, command)
+	if command == "AND"
+		bothwords(invidx, word1, word2)
+	elseif command == "OR"
+		anyword(invidx, word1, word2)
+	elseif command == "NOT"
+		withoutsecond(invidx, word1, word2)
+	else 
+		[]
+	end
+end
+
+# ╔═╡ 4053ee14-914f-11eb-2780-f75fc25a13b3
+booleansearch(invidx, word1, word2, command)
+
+# ╔═╡ 35ef1e42-9151-11eb-0179-8f8afd3e55f8
+
+
 # ╔═╡ Cell order:
 # ╠═9170c1a6-913f-11eb-0d5e-41f097fd770f
+# ╠═f70b8200-914c-11eb-1e9e-f31b3927d722
 # ╠═a36cdab6-913f-11eb-3356-c979b7b5c91f
 # ╠═ccdb279e-9140-11eb-3d8b-bd419f3c6d41
 # ╠═c5624a56-9140-11eb-38dd-033c51745656
@@ -70,3 +145,16 @@ saveindex(invidx)
 # ╠═a377575a-9141-11eb-10f0-d3b6e743f731
 # ╠═86a0a130-9147-11eb-311e-974ce98d39c8
 # ╠═d18d5204-9147-11eb-102d-c1c036f4330d
+# ╠═60550df8-914d-11eb-2eba-992e3beb53dd
+# ╠═6f655a78-914d-11eb-2769-dd25486211e3
+# ╠═d7ac1296-914d-11eb-359e-03a5a5b346a7
+# ╠═9d3e7646-914d-11eb-1a72-6ffa5fb430fb
+# ╠═eae3e746-914d-11eb-11d2-9f4bc8e0fdf9
+# ╠═05d0beb2-914e-11eb-2203-f792beaa3ac0
+# ╠═0d796f24-914e-11eb-044e-a3c821a44dd9
+# ╠═639624ce-914e-11eb-3e9f-d97221ea109e
+# ╠═58b4c9ce-914f-11eb-3cbf-99c436e2e293
+# ╠═bceedcd6-914f-11eb-3337-954c38a501ae
+# ╠═e122ed88-914d-11eb-0350-212db87d2f94
+# ╠═4053ee14-914f-11eb-2780-f75fc25a13b3
+# ╠═35ef1e42-9151-11eb-0179-8f8afd3e55f8
