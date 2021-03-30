f79a240bf38ee5dc5a778969195207db2988ac12
flisoch <flisochcs@gmail.com>
2021-03-30 15:19:51 +0300

    add boolean search examples

--- task3 - inverted index/Task3.jl
+++ task3 - inverted index/Task3.jl
@@ -69,27 +69,6 @@ end
 # ╔═╡ d18d5204-9147-11eb-102d-c1c036f4330d
 saveindex(invidx)
 
-# ╔═╡ 60550df8-914d-11eb-2eba-992e3beb53dd
-@bind query TextField()
-
-# ╔═╡ 6f655a78-914d-11eb-2769-dd25486211e3
-splitquery = split(query)
-
-# ╔═╡ d7ac1296-914d-11eb-359e-03a5a5b346a7
-command = splitquery[2]
-
-# ╔═╡ 9d3e7646-914d-11eb-1a72-6ffa5fb430fb
-invidx[splitquery[1]]
-
-# ╔═╡ eae3e746-914d-11eb-11d2-9f4bc8e0fdf9
-totalnums = []
-
-# ╔═╡ 05d0beb2-914e-11eb-2203-f792beaa3ac0
-word1 = splitquery[1]
-
-# ╔═╡ 0d796f24-914e-11eb-044e-a3c821a44dd9
-word2 = splitquery[3]
-
 # ╔═╡ 639624ce-914e-11eb-3e9f-d97221ea109e
 function bothwords(invidx, word1, word2)
 	list1 = invidx[word1]
@@ -125,11 +104,35 @@ function booleansearch(invidx, word1, word2, command)
 	end
 end
 
+# ╔═╡ b72ac27c-9151-11eb-30c5-bf8ae3348d04
+@bind query TextField()
+
+# ╔═╡ c25504f0-9151-11eb-26a7-cba04e3373cc
+splitquery = split(query)
+
+# ╔═╡ c5feb540-9151-11eb-018a-0d274c8f6ee2
+command = splitquery[2]
+
+# ╔═╡ caf1b090-9151-11eb-10a2-f925d1dae2ba
+word1 = splitquery[1]
+
+# ╔═╡ da9373b2-9151-11eb-2def-5b7c5b928240
+word2 = splitquery[3]
+
+# ╔═╡ 11f87974-9152-11eb-06dd-2d769e955f27
+invidx[word1]
+
+# ╔═╡ 13ba956c-9152-11eb-2fc9-51df80f75015
+invidx[word2]
+
 # ╔═╡ 4053ee14-914f-11eb-2780-f75fc25a13b3
 booleansearch(invidx, word1, word2, command)
 
 # ╔═╡ 35ef1e42-9151-11eb-0179-8f8afd3e55f8
+booleansearch(invidx, word1, word2, "AND")
 
+# ╔═╡ 286d7fec-9152-11eb-33d3-756c2a8f991d
+booleansearch(invidx, word1, word2, "OR")
 
 # ╔═╡ Cell order:
 # ╠═9170c1a6-913f-11eb-0d5e-41f097fd770f
@@ -145,16 +148,17 @@ booleansearch(invidx, word1, word2, command)
 # ╠═a377575a-9141-11eb-10f0-d3b6e743f731
 # ╠═86a0a130-9147-11eb-311e-974ce98d39c8
 # ╠═d18d5204-9147-11eb-102d-c1c036f4330d
-# ╠═60550df8-914d-11eb-2eba-992e3beb53dd
-# ╠═6f655a78-914d-11eb-2769-dd25486211e3
-# ╠═d7ac1296-914d-11eb-359e-03a5a5b346a7
-# ╠═9d3e7646-914d-11eb-1a72-6ffa5fb430fb
-# ╠═eae3e746-914d-11eb-11d2-9f4bc8e0fdf9
-# ╠═05d0beb2-914e-11eb-2203-f792beaa3ac0
-# ╠═0d796f24-914e-11eb-044e-a3c821a44dd9
 # ╠═639624ce-914e-11eb-3e9f-d97221ea109e
 # ╠═58b4c9ce-914f-11eb-3cbf-99c436e2e293
 # ╠═bceedcd6-914f-11eb-3337-954c38a501ae
 # ╠═e122ed88-914d-11eb-0350-212db87d2f94
+# ╠═b72ac27c-9151-11eb-30c5-bf8ae3348d04
+# ╠═c25504f0-9151-11eb-26a7-cba04e3373cc
+# ╠═c5feb540-9151-11eb-018a-0d274c8f6ee2
+# ╠═caf1b090-9151-11eb-10a2-f925d1dae2ba
+# ╠═da9373b2-9151-11eb-2def-5b7c5b928240
+# ╠═11f87974-9152-11eb-06dd-2d769e955f27
+# ╠═13ba956c-9152-11eb-2fc9-51df80f75015
 # ╠═4053ee14-914f-11eb-2780-f75fc25a13b3
 # ╠═35ef1e42-9151-11eb-0179-8f8afd3e55f8
+# ╠═286d7fec-9152-11eb-33d3-756c2a8f991d
