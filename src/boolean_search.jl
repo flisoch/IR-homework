606052b25f87184bff73b842a00279cb1233c1c1
flisoch <flisochcs@gmail.com>
2021-04-02 03:33:58 +0300

    remove fruitless code in boolean search and unused vars in inverse_index

--- src/boolean_search.jl
+++ src/boolean_search.jl
@@ -29,7 +29,7 @@ InverseIndex = ingredients("inverse_index.jl")
 corpus = InverseIndex.loadtexts([x for x=1:98])
 
 # ╔═╡ 40f7ff8c-3d9d-4f50-accc-34b0db05652c
-corpus.lexicon["gautama"]
+# corpus.lexicon["gautama"]
 
 # ╔═╡ f368dac6-2ced-4077-ae57-10576264eac2
 invidx = InverseIndex.saved_inverseindex()
@@ -40,35 +40,8 @@ function withoutsecond(list1, list2)
 	findall(x -> (x in list1) && !(x in list2), both)
 end
 
-# ╔═╡ e08f1917-b5ad-4dd1-85cb-c47eff10e5ff
-function bothwords(invidx, word1, word2)
-	list1 = invidx[word1]
-	list2 = invidx[word2]
-	intersect(list1, list2)
-end
-
-# ╔═╡ e4ddb8af-4143-448f-9973-eaf9de669cbc
-function anyword(invidx, word1, word2)
-	list1 = invidx[word1]
-	list2 = invidx[word2]
-	unique([list1; list2])
-end
-
-# ╔═╡ 683c4ac4-95b1-4276-8c83-dddbd5e6c141
-function booleansearch(invidx, word1, word2, command)
-	if command == "AND"
-		bothwords(invidx, word1, word2)
-	elseif command == "OR"
-		anyword(invidx, word1, word2)
-	elseif command == "NOT"
-		withoutsecond(invidx, word1, word2)
-	else 
-		[]
-	end
-end
-
 # ╔═╡ c28d26c9-c18f-41cd-9286-035ab0f7f56b
-query = "belief AND god NOT gautama AND buddha"
+query = "buddha AND gautama"
 
 # ╔═╡ 8702638f-ab5c-4e45-87f0-7795dfb45476
 s = split(query)
@@ -79,8 +52,6 @@ function booleansearch(invidx, query)
 	result = invidx[s[1]]
 		
 	for i = 2:length(s)
-		@show i
-		@show s[i]
 		if (i != length(s))
 			nextword = s[i+1]
 			if s[i] == "AND"
@@ -98,70 +69,6 @@ end
 # ╔═╡ 196c68e1-0fbe-4858-9fac-e1851e51f756
 booleansearch(invidx, query)
 
-# ╔═╡ 7a3fb5c4-19b9-4820-8561-32ad73014e5c
-ands = findall(x -> x=="AND", s)
-
-# ╔═╡ e58b75c2-6ab6-4f2c-bab8-caccded8d1fa
-ors = findall(x -> x=="OR", s)
-
-# ╔═╡ 9950203f-56e0-4120-a907-182041cdf4a0
-nots = findall(x -> x=="NOT", s)
-
-# ╔═╡ 43ac6ee9-d3a9-45a5-ac78-e8345b7d88e4
-ands_stack = Stack{Int}()
-
-# ╔═╡ 4971b8d8-78d9-4f4a-ad51-1d789cd454eb
-ors_stack = Stack{Int}()
-
-# ╔═╡ 0953e184-f245-4061-97ea-eb22db501bd4
-nots_stack = Stack{Int}()
-
-# ╔═╡ a874e867-f05e-4b92-bfd2-f2ce54e7e326
-queue = Queue{Stack}()
-
-# ╔═╡ fbadb8f4-d763-4b74-a4de-a5d80af7fc86
-begin 
-	for index in ands
-		push!(ands_stack, index)
-	end
-
-	for index in ors
-		push!(ors_stack, index)
-	end
-	
-	for index in nots
-		push!(nots_stack, index)
-	end
-	
-	enqueue!(queue, ands_stack)
-	enqueue!(queue, ors_stack)
-	enqueue!(queue, nots_stack)
-end
-
-# ╔═╡ f390659f-5732-4cb8-8293-297b0aa0166b
-used = [] 
-
-# ╔═╡ 7301aa3f-db2f-49a4-aa4b-9a20d9e58b0e
-isempty(queue)
-
-# ╔═╡ 77669d0b-bb76-42d8-97da-9ddb87302562
-totalqueue = Queue{Array}()
-
-# ╔═╡ 002fb40a-460c-46ee-b490-1e77a22dacc6
-while !isempty(queue)
-	stack = dequeue!(queue)
-	while !isempty(stack)
-		idx = pop!(stack)
-		if !(idx - 1 in used)
-			word = s[idx-1]
-			result1 = invidx[word]
-		end
-		push!(used, idx - 1)
-		push!(used, idx)
-		push!(used, idx + 1)
-	end
-end
-
 # ╔═╡ Cell order:
 # ╠═b985c3a2-bdee-4d23-9de7-fa5e56670153
 # ╠═5f6091d0-932d-11eb-1ff2-81e7f2a1a69a
@@ -169,23 +76,8 @@ end
 # ╠═33c22acb-5b4d-4fd7-bbb7-7ec13ed1bad7
 # ╠═40f7ff8c-3d9d-4f50-accc-34b0db05652c
 # ╠═f368dac6-2ced-4077-ae57-10576264eac2
-# ╠═683c4ac4-95b1-4276-8c83-dddbd5e6c141
 # ╠═7f3a0391-2010-421f-a20c-76e3ecacf92a
-# ╠═e08f1917-b5ad-4dd1-85cb-c47eff10e5ff
-# ╠═e4ddb8af-4143-448f-9973-eaf9de669cbc
 # ╠═c28d26c9-c18f-41cd-9286-035ab0f7f56b
 # ╠═8702638f-ab5c-4e45-87f0-7795dfb45476
 # ╠═334ffc1c-4992-4306-9c78-ddde65ae542b
 # ╠═196c68e1-0fbe-4858-9fac-e1851e51f756
-# ╠═7a3fb5c4-19b9-4820-8561-32ad73014e5c
-# ╠═e58b75c2-6ab6-4f2c-bab8-caccded8d1fa
-# ╠═9950203f-56e0-4120-a907-182041cdf4a0
-# ╠═43ac6ee9-d3a9-45a5-ac78-e8345b7d88e4
-# ╠═4971b8d8-78d9-4f4a-ad51-1d789cd454eb
-# ╠═0953e184-f245-4061-97ea-eb22db501bd4
-# ╠═fbadb8f4-d763-4b74-a4de-a5d80af7fc86
-# ╠═a874e867-f05e-4b92-bfd2-f2ce54e7e326
-# ╠═f390659f-5732-4cb8-8293-297b0aa0166b
-# ╠═7301aa3f-db2f-49a4-aa4b-9a20d9e58b0e
-# ╠═77669d0b-bb76-42d8-97da-9ddb87302562
-# ╠═002fb40a-460c-46ee-b490-1e77a22dacc6
---
+++ src/inverse_index.jl
@@ -0,0 +1,89 @@
+### A Pluto.jl notebook ###
+# v0.14.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7c8adb4b-7039-48b7-81a5-21bcd63984fb
+using TextAnalysis
+
+# ╔═╡ 78387644-932d-11eb-33e1-47040dd4d2e4
+function loadtexts(indices, path="../data/texts/")
+	texts = []
+	for i = 1:length(indices)
+		filepath = path * string(indices[i]) * ".txt"
+		if isfile(filepath)
+			@show filepath
+			io = open(filepath, "r")
+			text = readline(io)
+			push!(texts, StringDocument(text))
+			close(io)
+		end
+	end
+	Corpus(texts)
+end
+			
+
+# ╔═╡ e8bcf9bc-bc40-4f91-aed2-0de5c09f03a3
+# for test. Republic, Philosophy, Gautama Buddha
+# texts = loadtexts([32, 78, 82])
+# sd = StringDocument(texts[2])
+
+# ╔═╡ a7f3db01-1759-45f2-abf2-de2e78196811
+function inverseindex(corpus)
+	update_lexicon!(corpus)
+	update_inverse_index!(corpus)
+	inverse_index(corpus)
+end
+
+# ╔═╡ 51297f05-60db-4091-99b8-273acaa8b85a
+function saved_inverseindex(path = "../data/inverted_index.txt")
+	invidx = Dict()
+	io = open(path, "r")
+	lines = readlines(io)
+	for line in lines
+		s = split(line, " ")
+		word = s[1]
+		nums = []
+		@show line
+		@show word
+		for i=2:length(s)
+			if (s[i] != "")
+				push!(nums, parse(Int32, s[i]))
+			end
+		end
+		invidx[word] = nums
+	end
+	invidx
+end
+
+# ╔═╡ 04d7b0ff-7554-4aac-9cd0-12a7b4cdcf90
+corpus = loadtexts([x for x=1:97])
+
+# ╔═╡ caf285b0-b1d4-4731-b55e-9cf9be61d579
+# update_inverse_index!(corpus)
+
+# ╔═╡ 8ab2244f-06ec-4ead-9d31-4a61d32ea2c9
+update_lexicon!(corpus)
+
+# ╔═╡ 53e435dc-94e1-4d44-98e4-f44cedf4ba97
+update_inverse_index!(corpus)
+
+# ╔═╡ 71640944-8081-4fb2-92be-3687238eff06
+invidx = saved_inverseindex()
+
+# ╔═╡ 50dd6751-3d52-4203-936a-fd4410e073d6
+invidx["gautama"]
+
+# ╔═╡ Cell order:
+# ╠═7c8adb4b-7039-48b7-81a5-21bcd63984fb
+# ╠═78387644-932d-11eb-33e1-47040dd4d2e4
+# ╠═e8bcf9bc-bc40-4f91-aed2-0de5c09f03a3
+# ╠═a7f3db01-1759-45f2-abf2-de2e78196811
+# ╠═51297f05-60db-4091-99b8-273acaa8b85a
+# ╠═04d7b0ff-7554-4aac-9cd0-12a7b4cdcf90
+# ╠═caf285b0-b1d4-4731-b55e-9cf9be61d579
+# ╠═8ab2244f-06ec-4ead-9d31-4a61d32ea2c9
+# ╠═53e435dc-94e1-4d44-98e4-f44cedf4ba97
+# ╠═71640944-8081-4fb2-92be-3687238eff06
+# ╠═50dd6751-3d52-4203-936a-fd4410e073d6
